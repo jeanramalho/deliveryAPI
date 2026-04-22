c83e5af7760d51c193cac80d099d790d963ccda8
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-04-22 20:34:50 -0300

    [feat] - Cria clase Cliente

--- src/main/java/deliveryTech/deliveryAPI/Model/Cliente.java
+++ src/main/java/deliveryTech/deliveryAPI/Model/Cliente.java
@@ -1,5 +1,50 @@
 package deliveryTech.deliveryAPI.Model;
 
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String telefone;
+    private String endereco;
+
+    @Column(unique = true)
+    private String email;
+
+    @Builder.Default
+    private Boolean ativo = true;
+
+    @Builder.Default
+    private LocalDateTime dataCriacao = LocalDateTime.now();
+
+    @JsonIgnore
+    public void inativar() {
+        this.ativo = false;
+    }
+
+    public Cliente(){
+        
+    }
 
 }
