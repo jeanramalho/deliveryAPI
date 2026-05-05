a80d4647a9f7ab5560afe93b8302934f1598a063
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-05-04 21:15:37 -0300

    [feat] - Finaliza o ItemPedidoResponse

---
+++ src/main/java/deliveryTech/deliveryAPI/dto/response/ItemPedidoResponse.java
@@ -0,0 +1,17 @@
+package deliveryTech.deliveryAPI.dto.response;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemPedidoResponse {
+    private Long produtoId;
+    private String nomeProduto;
+    private Integer quantidade;
+    private BigDecimal precoUnitario;
+}
